86587defa6bc8c087f444e1a989b8ae202532812
Maxim Tacu <mtacu@tacitknowledge.com>
2017-05-15 14:49:27 +0300

    changed image

--- google-two-tier/main.tf
+++ google-two-tier/main.tf
@@ -36,7 +36,7 @@ resource "google_compute_instance" "www" {
   tags         = ["www-node"]
 
   disk {
-    image = "centos-7-v20170426"
+    image = "centos-7/centos-7-v20170426"
   }
 
   network_interface {
